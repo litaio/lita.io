899404be3e92625b2e45d2e4b4ed7d2e3df833a2
Jimmy Cuadra <jimmy@jimmycuadra.com>
2016-02-25 07:12:27 -0800

    Fix subtle S3 sync issues.

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ bundle exec ruby -Ilib -e 'require "plugin_updater"; PluginUpdater.update'
 bundle exec middleman build
 
 cd build
-aws s3 sync . s3://static.lita.io --delete --exclude '*' --include 'assets' --include 'images' --include 'javascripts' --include 'stylesheets'
+aws s3 sync . s3://static.lita.io --delete --exclude '*' --include 'assets/*' --include 'images/*' --include 'javascripts/*' --include 'stylesheets/*'
 
 cd docs
 aws s3 sync . s3://docs.lita.io --content-type 'text/html; charset=utf-8' --delete
@@ -19,7 +19,7 @@ cd ../plugins
 aws s3 sync . s3://plugins.lita.io --content-type 'text/html; charset=utf-8' --delete
 
 cd ../www
-aws s3 sync . s3://www.lita.io --content-type 'text/html; charset=utf-8' --delete
-aws s3api put-object --bucket 'www.lita.io' --key 'plugins/index.html' --metadata 'x-amz-website-redirect-location=https://plugins.lita.io/'
+aws s3 sync . s3://www.lita.io --content-type 'text/html; charset=utf-8' --delete --exclude 'plugins/*'
+aws s3 sync . s3://www.lita.io --exclude '*' --include 'plugins/*' --website-redirect 'https://plugins.lita.io/'
 
 echo 'lita.io deployed!'
