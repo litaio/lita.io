acd4fe9360b9d762102e858fb04d17d08d0ca098
Jimmy Cuadra <jimmy@jimmycuadra.com>
2016-02-25 05:31:24 -0800

    Set Content-Type for HTML and add plugins page redirect.

--- deploy.sh
+++ deploy.sh
@@ -17,12 +17,13 @@ aws s3 sync javascripts s3://static.lita.io
 aws s3 sync stylesheets s3://static.lita.io
 
 cd docs
-aws s3 sync . s3://docs.lita.io
+aws s3 sync . s3://docs.lita.io --content-type 'text/html; charset=utf-8' --delete
 
 cd ../plugins
-aws s3 sync . s3://plugins.lita.io
+aws s3 sync . s3://plugins.lita.io --content-type 'text/html; charset=utf-8' --delete
 
 cd ../www
-aws s3 sync . s3://www.lita.io
+aws s3 sync . s3://www.lita.io --content-type 'text/html; charset=utf-8' --delete
+aws s3api put-object --bucket 'www.lita.io' --key 'plugins/index.html' --metadata 'x-amz-website-redirect-location=https://plugins.lita.io/'
 
 echo 'lita.io deployed!'
